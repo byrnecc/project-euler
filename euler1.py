c1a4213a02659c83edeb815d2ce78c977ee9f9e7
Chris Byrne <byrnecc@gmail.com>
2018-03-21 18:43:23 +1100

    tidied up some scripts

--- euler1.py
+++ euler1.py
@@ -1,5 +1,9 @@
+#
+# Project Euler problem 1
+#
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
+#
 #
 # First define our range of numbers to loop through
 varTotal=0
@@ -9,6 +13,7 @@ for num in range(0,1000):
 	# if true add to our running total
 		varTotal += num
 		print(num, 'is divisible by 3 or 5! adding to total')
+		print('Total is now:',varTotal)
 #at completion of loop, print the running total	
-print(varTotal)
-	
+print('Grand total is:', varTotal)
+exit()	
