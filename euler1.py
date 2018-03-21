ed390c62f832d0d0c2984f4f2959b7b8594de883
Chris Byrne <byrnecc@gmail.com>
2018-03-21 18:30:10 +1100

    Add first two solutions

---
+++ euler1.py
@@ -0,0 +1,14 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+#
+# First define our range of numbers to loop through
+varTotal=0
+for num in range(0,1000):
+	# check if divisible by 3 or 5
+	if num % 3 == 0 or num % 5 == 0:
+	# if true add to our running total
+		varTotal += num
+		print(num, 'is divisible by 3 or 5! adding to total')
+#at completion of loop, print the running total	
+print(varTotal)
+	
